b6545e743e7384175f9754e0751b6a67a43ac839
Simranbal04 <simranbal01@gmail.com>
2020-03-19 15:20:55 -0400

    Got URL Data in console

--- app/src/main/java/com/simrankaurbal/newsreaderapp/MainActivity.java
+++ app/src/main/java/com/simrankaurbal/newsreaderapp/MainActivity.java
@@ -2,13 +2,77 @@ package com.simrankaurbal.newsreaderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,titles);
+        listView.setAdapter(arrayAdapter);
+        DownloadTask downloadTask = new DownloadTask();
+        try {
+            downloadTask.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
+
+
+    // content of url download
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String results = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            try {
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+                while (data != -1){
+                    char current = (char) data;
+                    results += current;
+                    data = inputStreamReader.read();
+                }
+                Log.i("UrlConnection",results);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
